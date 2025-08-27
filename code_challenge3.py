085a9cb919c323036be4149beace4474b18a8f5e
Izumi-cie <chrlsaciesrrn@gmail.com>
2025-08-27 11:21:16 +0800

    Add files via upload

---
+++ code_challenge3.py
@@ -0,0 +1,14 @@
+from getpass import getpass
+username = input("Input usename -> ")
+password = input("Enter password -> ")
+
+uname = 'acie'
+pwd = 'punongsaging'
+
+if uname == username and password == pwd :
+	print("Access Granted")
+
+else:
+	print("Access Denied")
+
+
